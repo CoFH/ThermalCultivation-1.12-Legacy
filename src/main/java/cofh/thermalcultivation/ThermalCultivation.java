a04287d6d45fdb4e6d8a77ecf9be4bea8ca5177d
King Lemming <kinglemming@gmail.com>
2018-09-19 01:20:59 -0400

    Update build.properties
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalcultivation/ThermalCultivation.java
+++ src/main/java/cofh/thermalcultivation/ThermalCultivation.java
@@ -29,7 +29,7 @@ public class ThermalCultivation {
 	public static final String MOD_ID = "thermalcultivation";
 	public static final String MOD_NAME = "Thermal Cultivation";
 
-	public static final String VERSION = "0.3.0";
+	public static final String VERSION = "0.3.1";
 	public static final String VERSION_MAX = "1.0.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
